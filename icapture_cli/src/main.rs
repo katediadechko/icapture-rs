0f6e789e70f1741eaedda7fed5824f8ee7bffb5d
Kate Nyzhnyk <kdiadechko@gmail.com>
2024-08-26 20:40:54 +0200

    Add quick preview

--- icapture_cli/src/main.rs
+++ icapture_cli/src/main.rs
@@ -2,7 +2,9 @@ use icapture_core::ICapture;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let icapture = ICapture::new("USB Capture HDMI 4K+")?;
+    let mut icapture = ICapture::new("USB Capture HDMI 4K+")?;
+
+    icapture.preview()?;
 
     Ok(())
 }
--- icapture_core/src/lib.rs
+++ icapture_core/src/lib.rs
@@ -1,13 +1,20 @@
-use opencv::{videoio, videoio::VideoCapture, Result};
+use opencv::{
+    highgui,
+    prelude::*,
+    videoio::{self, VideoCapture, VideoCaptureTraitConst},
+    Result,
+};
 
 pub mod device;
 
 pub struct ICapture {
+    device_name: String,
     instance: VideoCapture,
 }
 
 impl ICapture {
     pub fn new(name: &str) -> Result<Self, String> {
+        let device_name: String = name.to_string();
         let device_id = match Self::find_device_by_name(name) {
             Some(id) => id as i32,
             None => return Err(format!("Cannot find capture device '{name}'")),
@@ -18,10 +25,39 @@ impl ICapture {
             Err(error) => return Err(error.message),
         };
 
-        Ok(Self { instance })
+        match instance.is_opened() {
+            Ok(is_opened) => {
+                if !is_opened {
+                    return Err(format!("Cannot open capture device '{name}'"));
+                }
+            }
+            Err(error) => {
+                return Err(error.message);
+            }
+        };
+
+        Ok(Self { device_name, instance })
     }
 
-    pub fn dispose(_instance: &VideoCapture) {}
+    pub fn dispose(_instance: &VideoCapture) {
+    }
+
+    pub fn preview(&mut self) -> Result<()> {
+        let window: &str = self.device_name.as_str();
+        highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+        loop {
+            let mut frame = Mat::default();
+            self.instance.read(&mut frame)?;
+            if frame.size()?.width > 0 {
+                highgui::imshow(window, &frame)?;
+            }
+            let key = highgui::wait_key(10)?;
+            if key > 0 && key != 255 {
+                break;
+            }
+        }
+        Ok(())
+    }
 
     fn find_device_by_name(name: &str) -> Option<usize> {
         let devices = device::enumerate_capture_devices().ok()?;
