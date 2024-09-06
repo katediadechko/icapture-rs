a87e73abfaacc6f86aa8af552436ad1f27c64f87
Kate Nyzhnyk <kdiadechko@gmail.com>
2024-09-06 17:16:11 +0200

    Add config tests

--- icapture_core/src/config.rs
+++ icapture_core/src/config.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io;
 use std::io::BufReader;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Config {
     pub device_name: String,
     pub fps: u32,
@@ -47,7 +47,10 @@ impl Config {
             Ok(config) => config,
             Err(_) => {
                 warn!("cannot read config file '{file_path}'");
-                warn!("falling back to default configuration {:?}", Self::default());
+                warn!(
+                    "falling back to default configuration {:?}",
+                    Self::default()
+                );
                 Self::default()
             }
         }
@@ -57,5 +60,35 @@ impl Config {
         let reader = BufReader::new(File::open(file_path)?);
         let config = serde_json::from_reader(reader)?;
         Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn test_from_valid_file_succeeds() {
+        let config = Config {
+            device_name: "Test device".to_string(),
+            fps: 42,
+            frame_width: 2560,
+            frame_height: 1440,
+        };
+        let json = serde_json::to_string(&config).unwrap();
+        let file_path = "test_config.json";
+        fs::write(file_path, json).unwrap();
+        let loaded_config = Config::from_file(file_path);
+        assert_eq!(loaded_config, config);
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_from_invalid_file_defaults() {
+        let file_path = "invalid_test_config.json";
+        let loaded_config = Config::from_file(file_path);
+        assert_eq!(loaded_config, Config::default());
     }
 }
